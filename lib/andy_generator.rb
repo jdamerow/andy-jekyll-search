a46dff3b4bbc3c4a8a2206f581d7eb818aee1874
Julia Damerow <jdamerow@users.noreply.github.com>
2020-07-13 12:39:08 -0400

    Update andy_generator.rb

--- lib/andy_generator.rb
+++ lib/andy_generator.rb
@@ -1,23 +1,23 @@
 require 'json'
+require 'fileutils'
 
 module Jekyll
   class AndyGenerator < Jekyll::Generator
-    
+
     SEARCH_INDEX_NAME = "search-index.js"
     LIST_FILE = "catalog.json"
     IDX_TEMPLATE_STRING = "lunr_index.add({ url: '{#id}', title: {#title}, body: {#body}})"
-    
+
     def generate(site)
       return if site.pages.empty?
-      
+
       js_file_name = File.expand_path(File.join(File.dirname(__FILE__), "search-index.js"))
-      
+
       js_file = open(js_file_name)
       js_content = js_file.read
-      
+
       index_entries = [js_content]
       json_entries = []
-
       site.posts.each_with_index do |post, index|
         entry = IDX_TEMPLATE_STRING.sub "{#id}", post.url
         entry.sub! "{#title}", post.title.to_json
@@ -26,16 +26,16 @@ module Jekyll
 
         json_entries << "{ \"title\":"  + post.title.to_json + ", \"url\":\"" + post.url + "\", \"excerpt\":"  + post.excerpt.to_json + " }"
       end
-      
-      page_path = site.pages.first.path
-      posts_dir = File.dirname(page_path)
-      assets_dir = File.expand_path(posts_dir + "/../assets")
+
+      posts_dir = File.dirname(".")
+      assets_dir = File.expand_path(posts_dir + "/assets")
       
       generated_js_file = assets_dir + "/js/" + SEARCH_INDEX_NAME
-      
+      FileUtils.mkdir_p assets_dir + "/js/"
+
       g_js_file = File.open generated_js_file, "w"
       g_js_file.puts index_entries.join("\n")
-      
+
       g_js_file.close
 
       # create entries
